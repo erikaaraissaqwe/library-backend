9926bdc2b2604beb65c308bf33533714bc8172bb
Erika Raissa Bueno da Silva <erikaaraissaqwe@gmail.com>
2021-11-27 11:38:09 -0300

    Crud do book - delete, update, listOne, listAll

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -28,10 +28,82 @@ exports.register = (req, res) => {
     });
 
     book.save(book).then(data => {
-      res.send({ data })
+      res.send({ data });
     }).catch(err => {
         res.status(500).send({id: 'internal-error', msg: err.message });
     });
 }
 
+exports.delete = (req, res) => {
+  const id = req.params.id;
 
+  if(!id){
+    res.status(400).send({id: 'missing-data', msg: "Dados para a deleção insuficientes." });
+    return;
+  }
+
+  Book.findByIdAndRemove(id).then(data =>{
+    if(!data){
+      res.status(400).send({id: 'internal-error', msg: "Não foi possível remover o livro" });
+    }
+    else{
+      res.send({ data });
+    }
+  }).catch(err => {
+    res.status(500).send({id: 'internal-error', msg: err.message });
+  });
+}
+
+exports.update = (req, res) => {
+  if (!(isNotEmpty(req.body.title) && 
+  req.body.authors.length > 0 &&
+  isNotEmpty(req.body.dateOfPublication) &&
+  req.body.pages &&
+  isNotEmpty(req.body.isbn) &&
+  isNotEmpty(req.body.resume))) {
+      
+    res.status(400).send({id: 'missing-data', msg: "Dados para a atualização insuficientes." });
+    return;
+  }
+
+  const id = req.params.id;
+
+  if(!id){
+    res.status(400).send({id: 'missing-data', msg: "Dados para a atualização insuficientes." });
+    return;
+  }
+
+  Book.findByIdAndUpdate(id, req.body).then(data =>{
+    if(!data){
+      res.status(400).send({id: 'book-not-found', msg: "Livro não cadastrado" });
+    }
+    else{
+      console.log(data);
+      res.send({ data });
+    }
+  }).catch(err => {
+    res.status(500).send({id: 'internal-error', msg: err.message });
+  });
+}
+
+exports.listAll = (req, res) => {
+  Book.find().then(data => {
+    res.send({ data });
+  }).catch(err => {
+    res.status(500).send({id: 'internal-error', msg: err.message });
+  });
+}
+
+exports.listOne = (req, res) => {
+  const id = req.params.id;
+
+  Book.findById(id).then(data => {
+      if (!data) {
+          res.status(404).send({id: 'book-not-found', msg: "Livro não cadastrado" });
+      } else {
+          res.send(data);
+      }
+  }).catch(err => {
+      res.status(500).send({id: 'internal-error', msg: err.message });
+  });
+};
--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -7,6 +7,14 @@ module.exports = app => {
     var router = require("express").Router();
     
     router.post('/register', authMiddleware.privateUser, book.register);
+
+    router.put('/:id', authMiddleware.privateUser, book.update);
+
+    router.get('/listAll', authMiddleware.privateUser, book.listAll);
+
+    router.get('/:id', authMiddleware.privateUser, book.listOne);
+
+    router.delete('/:id', authMiddleware.privateUser, book.delete);
     
     app.use('/api/book', router);
 };
